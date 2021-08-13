6d9ac626705bbbd564356d9763ad2c15773b6ee0
Zack West <43519801+alphazwest@users.noreply.github.com>
2021-08-13 11:18:25 -0400

    Create wilders_rsi_pandas.py
    An implementation of Welles Wilder's Relative Strength Index (RSI) calculation using the Pandas library.

---
+++ wilders_rsi_pandas.py
@@ -0,0 +1,56 @@
+def pandas_rsi(df: pd.DataFrame, window_length: int = 14, output: str = None):
+    """
+    An implementation of Wells Wilder's RSI calculation as outlined in
+    his 1978 book "New Concepts in Technical Trading Systems" which makes
+    use of the α-1 Wilder Smoothing Method of calculating the average
+    gains and losses across trading periods and the Pandas library.
+
+    @author: https://github.com/alphazwest
+    Args:
+        df: pandas.DataFrame - a Pandas Dataframe object
+        window_length: int - the period over which the RSI is calculated. Default is 14
+        output: str or None - optional output path to save data as CSV
+
+    Returns:
+        DataFrame object with columns as such, where xxx denotes an inconsequential
+        name of the provided first column:
+            ['xxx', 'diff', 'gain', 'loss', 'avg_gain', 'avg_loss', 'rs', 'rsi']
+    """
+    # Calculate Price Differences
+    df['diff'] = df.diff(1)
+
+    # Calculate Avg. Gains/Losses
+    df['gain'] = df['diff'].clip(lower=0).round(2)
+    df['loss'] = df['diff'].clip(upper=0).abs().round(2)
+
+    # Get initial Averages
+    df['avg_gain'] = df['gain'].rolling(window=window_length, min_periods=window_length).mean()[:window_length+1]
+    df['avg_loss'] = df['loss'].rolling(window=window_length, min_periods=window_length).mean()[:window_length+1]
+
+    # Calculate Average Gains
+    for i, row in enumerate(df['avg_gain'].iloc[window_length+1:]):
+        df['avg_gain'].iloc[i + window_length + 1] =\
+            (df['avg_gain'].iloc[i + window_length] *
+             (window_length - 1) +
+             df['gain'].iloc[i + window_length + 1])\
+            / window_length
+
+    # Calculate Average Losses
+    for i, row in enumerate(df['avg_loss'].iloc[window_length+1:]):
+        df['avg_loss'].iloc[i + window_length + 1] =\
+            (df['avg_loss'].iloc[i + window_length] *
+             (window_length - 1) +
+             df['loss'].iloc[i + window_length + 1])\
+            / window_length
+
+    # Calculate RS Values
+    df['rs'] = df['avg_gain'] / df['avg_loss']
+
+    # Calculate RSI
+    df['rsi'] = 100 - (100 / (1.0 + df['rs']))
+
+    # Save if specified
+    if output is not None:
+        df.to_csv(output)
+
+    return df
